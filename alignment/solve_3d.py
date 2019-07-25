f66920b20bba6b429a434f307238026693420d34
Dan Kapner <danielk@alleninstitute.org>
2019-07-25 12:46:18 -0700

    updating to fix transform

--- alignment/solve_3d.py
+++ alignment/solve_3d.py
@@ -12,13 +12,13 @@ example1 = {
             'actions': ['invert_opty'],
             'sd_set': {'src': 'em', 'dst': 'opt'}
         },
-        'output_json': '/allen/programs/celltypes/workgroups/em-connectomics/danielk/3D_alignment/tmp_out/transform.json',
+        'output_json': '/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_coregistration/tmp_out/transform.json',
         'model': 'TPS',
         'npts': 10,
         'regularization': {
             'translation': 1e0,
             'linear': 1e0,
-            'other': 1e20
+            'other': 1e-3
             }
 }
 example2 = {
@@ -28,7 +28,7 @@ example2 = {
             'actions': ['invert_opty'],
             'sd_set': {'src': 'opt', 'dst': 'em'}
         },
-        'output_json': '/allen/programs/celltypes/workgroups/em-connectomics/danielk/3D_alignment/tmp_out/transform.json',
+        'output_json': '/allen/programs/celltypes/workgroups/em-connectomics/danielk/em_coregistration/tmp_out/transform.json',
         'model': 'TPS',
         'npts': 10,
         'regularization': {
@@ -40,6 +40,23 @@ example2 = {
 
 
 def control_pts_from_bounds(data, npts):
+    """create thin plate spline control points
+    from the bounds of provided data.
+
+    Parameters
+    ----------
+    data : :class:`numpy.ndarray`
+        ndata x 3 Cartesian coordinates of data.
+    npts : int
+        number of control points per axis. total
+        number of control points will be npts^3
+
+    Returns
+    -------
+    control_pts : :class:`numpy.ndarray`
+        npts^3 x 3 Cartesian coordinates of controls.
+
+    """
     x, y, z = [
             np.linspace(data[:, i].min(), data[:, i].max(), npts)
             for i in [0, 1, 2]]
@@ -52,6 +69,27 @@ def control_pts_from_bounds(data, npts):
 
 
 def solve(A, w, r, dst):
+    """regularized linear least squares
+
+    Parameters
+    ----------
+    A : :class:`numpy.ndarray`
+        ndata x nparameter array
+    w : :class:`numpy.ndarray`
+        ndata x ndata diagonal weight matrix
+    r : :class:`numpy.ndarray`
+        nparameter x nparameter diagonal
+        regularization matrix
+    dst : :class:`numpy.ndarray`
+        ndata x 3 Cartesian coordinates
+        of transform destination.
+
+    Returns
+    -------
+    x : :class:`numpy.ndarray`
+        nparameter x 3 solution
+
+    """
     ATW = A.transpose().dot(w)
     K = ATW.dot(A) + r
     lu, piv = scipy.linalg.lu_factor(K, overwrite_a=True)
@@ -65,6 +103,22 @@ def solve(A, w, r, dst):
 
 
 def create_regularization(n, d):
+    """create diagonal regularization matrix
+
+    Parameters
+    ----------
+    n : int
+        number of parameters per Cartesian axis
+    d : dict
+        regularization dict from input
+
+    Returns
+    -------
+    R : :class:`numpy.ndarray`
+        n x n diagonal matrix containing regularization
+        factors
+        
+    """
     r = np.ones(n)
     r[0] = d['translation']
     r[1:3] = d['linear']
@@ -75,13 +129,39 @@ def create_regularization(n, d):
     return R
 
 
-def write_control_to_file(fpath, src, dst):
+def write_src_dst_to_file(fpath, src, dst):
+    """csv output of src and dst
+
+    Parameters
+    ----------
+    fpath : str
+        valid path
+    src : :class:`numpy.ndarray`
+        ndata x 3 source points
+    dst : :class:`numpy.ndarray`
+        ndata x 3 destination points
+
+    """
     out = np.hstack((src, dst))
     np.savetxt(fpath, out, fmt='%0.8e', delimiter=',')
     print('wrote %s' % fpath)
 
 
 def list_points_by_res_mag(res, labels, n=np.inf, factor=0.001):
+    """print to stdout point labels and residuals
+
+    Parameters
+    ----------
+    res : :class:`numpy.ndarray`
+        ndata x 3 residuals
+    labels : list
+        ndata length list of point labels
+    n : int
+        limit to print only highest n residuals
+    factor : float
+        scales the residuals
+
+    """
     mag = np.linalg.norm(res, axis=1)
     ind = np.argsort(mag)[::-1]
     i = 0
@@ -91,9 +171,20 @@ def list_points_by_res_mag(res, labels, n=np.inf, factor=0.001):
 
 
 class Solve3D(argschema.ArgSchemaParser):
+    """class to solve a 3D coregistration problem"""
     default_schema = SolverSchema
 
     def run(self, control_pts=None):
+        """run the solve
+
+        Parameters
+        ----------
+        control_pts : :class:`numpy.ndarray`
+            user-supplied ncntrl x 3 Cartesian coordinates
+            of control points. default None will create
+            control points from bounds of input data.
+
+        """
         d = DataLoader(input_data=self.args['data'], args=[])
         d.run()
         self.data = d.data
